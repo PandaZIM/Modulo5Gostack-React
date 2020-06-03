acd4dc6268d69f0be5671547e959d209142bca02
pandazim <jaotoledo74@gmail.com>
2020-06-03 14:08:39 -0400

    done

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import api from "../../services/api"
 
-// import { Container } from './styles';
+import Container from '../../components/Container'
+import { Owner, IssueList } from './styles'
+
 
 export default class Repository extends Component {
+  
   //armazenando data 
   state = {
     repository: {},
@@ -32,14 +36,39 @@ export default class Repository extends Component {
       repository: repository.data,
       issues: issues.data
     })
-    
-    console.log(repository)
-    console.log(issues)
-
   }
   
   render(){
-    return <h1> Repository</h1>;
+    const { repository, issues } = this.state
+    let owner = repository.owner
+    return (
+      <Container>
+          <Owner>
+            <Link to="/">Voltar aos reposítorios</Link>
+            
+            {owner ? <img src={owner.avatar_url} alt="Avatar"/> : null}
+            <h1>{repository.name}</h1>
+            <p>{repository.description}</p>
+          </Owner>
+
+          <IssueList>
+            {issues.map(issue => (
+              <li key={String(issue.id)}>
+                <img src={issue.user.avatar_url} alt={issue.user.login}></img>
+                <div>
+                  <strong>
+                    <a href={issue.html_url}>{issue.title}</a>
+                    {issue.labels.map(label => (
+                      <span key={String(label.id)}>{label.name}</span>
+                    ))}
+                  </strong>
+                  <p>{issue.user.login}</p>
+                </div>
+              </li>
+            ))}  
+          </IssueList> 
+      </Container>
+    );
   }
 }
 
