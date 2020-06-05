12c6b6a2f5350e3741ce9406176a36765adb47e0
pandazim <jaotoledo74@gmail.com>
2020-06-05 18:30:01 -0400

    Finalizado desafio

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import api from "../../services/api"
 
 import Container from '../../components/Container'
-import { Owner, IssueList } from './styles'
+import { Owner, IssueList, IssueFilter, PageActions } from './styles'
 
 
 export default class Repository extends Component {
@@ -11,13 +11,23 @@ export default class Repository extends Component {
   //armazenando data 
   state = {
     repository: {},
-    issues: []
+    issues: [],
+    //define os filtros que precisam existir
+    filters: [
+      { state: 'all', label: 'Todas', active: true},
+      { state: 'open', label: 'Abertas', active: false},
+      { state: 'closed', label: 'Fechadas', active: false},
+    ],
+    filterIndex: 0,
+    page: 1,
   }
   
   
   //pegando data
   async componentDidMount() {
     const { match } = this.props
+    //chamada dos filtros no state
+    const { filters } = this.state
 
     const repoName = decodeURIComponent(match.params.repository)
 
@@ -25,7 +35,7 @@ export default class Repository extends Component {
       api.get(`/repos/${repoName}`),
       api.get(`/repos/${repoName}/issues`, {
         params: {
-          state: 'open',
+          state: filters.find(f => f.active).state,
           per_page: 5,
         }
       }),
@@ -37,9 +47,42 @@ export default class Repository extends Component {
       issues: issues.data
     })
   }
+
+  //função para pegar as issues da api e pega os filtrado
+  loadIssues = async () => {
+    const { match } = this.props;
+    const { filters, filterIndex, page } = this.state
+    
+    const repoName = decodeURIComponent(match.params.repository)
+
+    const response = await api.get(`/repos${repoName}/issues`, {
+      params: {
+        state: filters[filterIndex].state,
+        per_page: 5,
+        page,
+      }
+    })
+
+    this.setState({ issues: response.data })
+  }
+
+  handlePage = async action => {
+    const { page } = this.state;
+    
+    await this.state({
+      page: action === 'back' ? page - 1 : page + 1
+    })
+
+    this.loadIssues()
+  }
+
+  handleFilterClick = async filterIndex => {
+    await this.setState({ filterIndex })
+    this.loadIssues()
+  }
   
   render(){
-    const { repository, issues } = this.state
+    const { repository, issues, filterIndex, filters, page } = this.state
     let owner = repository.owner
     return (
       <Container>
@@ -52,6 +95,16 @@ export default class Repository extends Component {
           </Owner>
 
           <IssueList>
+            <IssueFilter active={filterIndex}>
+              {filters.map((filter, map) => (
+                <button type="button"
+                key={filter.label}
+                onClick={() => this.handleFilterClick}
+                >
+
+                </button>
+              ))}
+            </IssueFilter>
             {issues.map(issue => (
               <li key={String(issue.id)}>
                 <img src={issue.user.avatar_url} alt={issue.user.login}></img>
@@ -66,7 +119,20 @@ export default class Repository extends Component {
                 </div>
               </li>
             ))}  
-          </IssueList> 
+          </IssueList>
+          <PageActions>
+            <button type="button"
+            disabled={page < 2}
+            onClick={() => this.handlePage('back')}
+            >Anterior
+            
+            </button>
+              <span> Página {page}</span>
+              <button type='button' 
+              onClick={() => this.handlePage('next')}>
+                Próximo
+              </button>
+            </PageActions> 
       </Container>
     );
   }
